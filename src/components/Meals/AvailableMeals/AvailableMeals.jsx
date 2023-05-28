4ef1b6021b8b2a503bd403e99d9b32c3430b478b
Yahia bassam <ybassam@2pegypt.local>
2023-05-28 15:45:03 +0300

    update | integrate with firebase

--- src/components/Meals/AvailableMeals/AvailableMeals.jsx
+++ src/components/Meals/AvailableMeals/AvailableMeals.jsx
@@ -1,49 +1,67 @@
-import classes from './AvailableMeals.module.css';
-import Card from '../../Shared/Card';
-import MealItem from '../MealItem/MealItem';
-
-const DUMMY_MEALS = [
-  {
-    id: 'm1',
-    name: 'Sushi',
-    description: 'Finest fish and veggies',
-    price: 20.00,
-  },
-  {
-    id: 'm2',
-    name: 'Schnitzel',
-    description: 'A german specialty!',
-    price: 30.00,
-  },
-  {
-    id: 'm3',
-    name: 'Barbecue Burger',
-    description: 'American, raw, meaty',
-    price: 12.99,
-  },
-  {
-    id: 'm4',
-    name: 'Green Bowl',
-    description: 'Healthy...and green...',
-    price: 18.99,
-  },
-];
+import classes from "./AvailableMeals.module.css";
+import Card from "../../Shared/Card";
+import MealItem from "../MealItem/MealItem";
+import { Spinner } from "react-bootstrap/";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const AvailableMeals = () => {
-  const Meals = DUMMY_MEALS.map( meal => {
-    return <MealItem key={meal.id} meal={meal} />
-  })
-  
-  
+  const [commingMeals, setCommingMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const baseURL = "https://react-meals-7b0d3-default-rtdb.firebaseio.com/meals.json";
+
+  useEffect(() => {
+    // Axios method
+    setIsLoading(true);
+    axios.get(baseURL)
+      .then((res) => {
+        setCommingMeals(Object.values(res.data));
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        setError(err.message);
+      });
+  }, []);
+
+  // useEffect(() => { // Fetch method
+  //   getMeals()
+  // }, [])
+  // const getMeals = async () => {
+  //   const response = await fetch("https://react-meals-7b0d3-default-rtdb.firebaseio.com/meals.json");
+  //   setCommingMeals( Object.values(await response.json()) );
+  // }
+
+  const Meals = commingMeals.map((meal) => {
+    return <MealItem key={meal.id} meal={meal} />;
+  });
+
   return (
     <section className={classes.meals}>
-      <Card>
-        <ul>
-          { Meals }
-        </ul>
-      </Card>
+      { error ?
+        <h5 style={{color:'red', textAlign: 'center'}}>{error}</h5>
+        : 
+        <div>
+          {isLoading ? (
+            <div className="d-flex flex-column justify-content-center align-items-center">
+              <div>
+                <Spinner animation="grow" variant="danger" className="me-3" />
+                <Spinner animation="grow" variant="danger" className="me-3" />
+                <Spinner animation="grow" variant="danger" className="me-3" />
+                <Spinner animation="grow" variant="danger" className="me-3" />
+              </div>
+              <h4 className="mt-3">Loading ...</h4>
+            </div>
+          ) : (
+            <Card>
+              <ul>{Meals}</ul>
+            </Card>
+          )}
+        </div>
+      }
+
     </section>
-  )
-}
+  );
+};
 
 export default AvailableMeals;
