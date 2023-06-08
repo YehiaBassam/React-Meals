9655b0a28bac9f60f1f3a8090b8659223e9ea32d
Yehia Bassam <yehiabassam94@gmail.com>
2023-06-08 15:41:26 +0300

    update | modify toast style

--- src/components/Shared/Toast/GlobalToast.module.jsx
+++ src/components/Shared/Toast/GlobalToast.module.jsx
@@ -1,27 +1,31 @@
 import React, { useContext } from 'react';
-import { Toast } from 'react-bootstrap';
+import { Toast, ToastContainer } from 'react-bootstrap';
 import { ToastContext } from '../../../store/Toast-Context';
-import classes from './GlobalToast.module.css';
 
 function GlobalToast() {
   const { isShowToast, closeToast, toastData } = useContext(ToastContext);
 
   return (
-    <Toast 
-      show={isShowToast} 
-      onClose={closeToast}
-      delay={3000} 
-      autohide 
-      bg={toastData.variant} 
-      className={classes['toast-wrapper']}
-      style={{ position: isShowToast && "absolute"}} 
+    <ToastContainer
+      className="p-3"
+      position='top-end'
+      style={{ zIndex: 10000 }}
     >
-      <Toast.Header>
-        <strong className="me-auto">{toastData.status}</strong>
-        <small>Now</small>
-      </Toast.Header>
-      <Toast.Body>{toastData.message}</Toast.Body>
-    </Toast>
+      <Toast
+        show={isShowToast}
+        onClose={closeToast}
+        bg={toastData.variant}
+        delay={3000}
+        autohide
+      >
+        <Toast.Header>
+          <strong className="me-auto">{toastData.status}</strong>
+          <small>Now</small>
+        </Toast.Header>
+        <Toast.Body>{toastData.message}</Toast.Body>
+      </Toast>
+
+    </ToastContainer>
   );
 }
 
